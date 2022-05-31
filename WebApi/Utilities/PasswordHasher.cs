fb82d48b81c1ad932181112fa48a6bc5d54756be
MK <komaei@live.com>
2022-05-31 20:29:24 +0430

    mdf ldf db files

--- WebApi/Utilities/PasswordHasher.cs
+++ WebApi/Utilities/PasswordHasher.cs
@@ -4,7 +4,6 @@ public class PasswordHasher
 {
     public const int Pbkdf2Iterations = 1000;
 
-
     public static string HashPasswordV3(string password)
     {
         return Convert.ToBase64String(HashPasswordV3(password, RandomNumberGenerator.Create()
@@ -12,6 +11,28 @@ public class PasswordHasher
             , numBytesRequested: 256 / 8));
     }
 
+    private static byte[] HashPasswordV3(string password, RandomNumberGenerator rng, KeyDerivationPrf prf, int iterCount, int saltSize, int numBytesRequested)
+    {
+        byte[] salt = new byte[saltSize];
+        rng.GetBytes(salt);
+        byte[] subkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, numBytesRequested);
+        var outputBytes = new byte[13 + salt.Length + subkey.Length];
+        outputBytes[0] = 0x01; // format marker
+        WriteNetworkByteOrder(outputBytes, 1, (uint)prf);
+        WriteNetworkByteOrder(outputBytes, 5, (uint)iterCount);
+        WriteNetworkByteOrder(outputBytes, 9, (uint)saltSize);
+        Buffer.BlockCopy(salt, 0, outputBytes, 13, salt.Length);
+        Buffer.BlockCopy(subkey, 0, outputBytes, 13 + saltSize, subkey.Length);
+        return outputBytes;
+    }
+
+    private static void WriteNetworkByteOrder(byte[] buffer, int offset, uint value)
+    {
+        buffer[offset + 0] = (byte)(value >> 24);
+        buffer[offset + 1] = (byte)(value >> 16);
+        buffer[offset + 2] = (byte)(value >> 8);
+        buffer[offset + 3] = (byte)(value >> 0);
+    }
 
     public static bool VerifyHashedPasswordV3(string hashedPasswordStr, string password)
     {
@@ -62,37 +83,11 @@ public class PasswordHasher
         }
     }
 
-
-    // privates
-    private static byte[] HashPasswordV3(string password, RandomNumberGenerator rng, KeyDerivationPrf prf, int iterCount, int saltSize, int numBytesRequested)
-    {
-        byte[] salt = new byte[saltSize];
-        rng.GetBytes(salt);
-        byte[] subkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, numBytesRequested);
-        var outputBytes = new byte[13 + salt.Length + subkey.Length];
-        outputBytes[0] = 0x01; // format marker
-        WriteNetworkByteOrder(outputBytes, 1, (uint)prf);
-        WriteNetworkByteOrder(outputBytes, 5, (uint)iterCount);
-        WriteNetworkByteOrder(outputBytes, 9, (uint)saltSize);
-        Buffer.BlockCopy(salt, 0, outputBytes, 13, salt.Length);
-        Buffer.BlockCopy(subkey, 0, outputBytes, 13 + saltSize, subkey.Length);
-        return outputBytes;
-    }
-
-    private static void WriteNetworkByteOrder(byte[] buffer, int offset, uint value)
-    {
-        buffer[offset + 0] = (byte)(value >> 24);
-        buffer[offset + 1] = (byte)(value >> 16);
-        buffer[offset + 2] = (byte)(value >> 8);
-        buffer[offset + 3] = (byte)(value >> 0);
-    }
-
     private static uint ReadNetworkByteOrder(byte[] buffer, int offset)
     {
         return ((uint)(buffer[offset + 0]) << 24)
             | ((uint)(buffer[offset + 1]) << 16)
             | ((uint)(buffer[offset + 2]) << 8)
-            | ((uint)(buffer[offset + 3]));
+            | buffer[offset + 3];
     }
-
 }
